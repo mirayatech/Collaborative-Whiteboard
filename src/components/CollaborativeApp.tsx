1ae65c7b6c4b586100bb8b5e9935b7c2410c671a
mirayatech <mirayaabrodi@gmail.com>
2025-01-18 18:29:41 +0100

    feat: Enhance CollaborativeApp with a canvas component and updated UI for displaying online users count

--- src/components/CollaborativeApp.tsx
+++ src/components/CollaborativeApp.tsx
@@ -1,9 +1,18 @@
 "use client";
 
 import { useOthers } from "@liveblocks/react/suspense";
+import { Canvas } from "./canvas";
 
 export function CollaborativeApp() {
   const others = useOthers();
   const userCount = others.length;
-  return <div>There are {userCount} other user(s) online</div>;
+
+  return (
+    <div className="relative w-full h-screen">
+      <div className="absolute top-4 right-4 bg-white px-4 py-2 rounded-full shadow-sm z-10">
+        {userCount} other user(s) online
+      </div>
+      <Canvas />
+    </div>
+  );
 }
